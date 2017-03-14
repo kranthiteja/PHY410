7cf0103e33f77a24836452e364cfef71066b7891
Kranthi Teja Buggineni <kranthit@horatio.public.buffalo.edu>
2017-03-13 23:32:33 -0400

    Problem2

---
+++ Homework5/Problem2.py
@@ -0,0 +1,16 @@
+from StudentRecord import StudentRecord
+import sys
+
+val = sys.argv[len(sys.argv) - 1]
+
+def main():
+ with open(val, "r") as ins:
+array = []
+n = 0
+sum = 0
+student.input(line)
+array.append(student)
+n = n+1
+sum = sum + float(student.score())
+average = sum/n
+print (average)
